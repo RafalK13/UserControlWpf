807e23858db83eb5c30c3c3d3ed386bda056b64d
RafalK13 <rafal.kaczewski@gmail.com>
2019-03-05 14:39:12 +0100

    Pierwszy zapis

---
+++ UserControlWPF/DaneRaf.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UserControlWPF
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public string city { get; set; }
+        public int age { get; set; }
+    }
+
+    public class DaneRaf
+    {
+        public ObservableCollection<Person> persons { get; set; }
+        public DataTable dtSourceRaf { get; set; }
+        public DataView dvSourceRaf { get; set; }
+
+        public DaneRaf()
+        {
+            init();
+        }
+
+        private void init()
+        {
+            persons = new ObservableCollection<Person>();
+            dtSourceRaf = new DataTable();
+            dvSourceRaf = new DataView(dtSourceRaf);
+            
+            initValues();
+        }
+
+        private void initValues()
+        {
+            initObservableList();
+            initDataTable();
+        }
+
+        private void initObservableList()
+        {
+            persons.Add(new Person() { name = "Rafałek", city = "Gdańsk", age = 47 });
+            persons.Add(new Person() { name = "Hanka", city = "Gdańsk", age = 11 });
+            persons.Add(new Person() { name = "Beti", city = "Sybir", age = 48 });
+        }
+
+        private void initDataTable()
+        {
+
+            dtSourceRaf.Columns.AddRange( new DataColumn[] 
+                                        { new DataColumn( "name", typeof(string)),
+                                          new DataColumn( "city", typeof(string)),
+                                          new DataColumn("age", typeof(int)) });
+
+            dtSourceRaf.Rows.Add("Rafał", "Gdańsk", 47);
+            dtSourceRaf.Rows.Add("Rafał1", "Gdańsk", 47);
+            dtSourceRaf.Rows.Add("Rafał2", "Gdańsk", 47);
+            dtSourceRaf.Rows.Add("Rafał3", "Gdańsk", 47);
+            dtSourceRaf.Rows.Add("Hanula", "Gdańsk", 11);
+            dtSourceRaf.Rows.Add("Hanula1", "Gdańsk", 11);
+            dtSourceRaf.Rows.Add("Hanula2", "Gdańsk", 11);
+            dtSourceRaf.Rows.Add("Hanula3", "Gdańsk", 11);
+            dtSourceRaf.Rows.Add("Beti", "Sybir", 48);
+            dtSourceRaf.Rows.Add("Beti1", "Sybir", 48);
+            dtSourceRaf.Rows.Add("Beti2", "Sybir", 48);
+            dtSourceRaf.Rows.Add("Beti3", "Sybir", 48);
+
+        }
+    }
+}
---
+++ UserControlWPF/UserControl1.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserControlWPF
+{
+    /// <summary>
+    /// Logika interakcji dla klasy UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        //zmienna wyświetlana w teksBox
+        public string TekstProp
+        {
+            get { return (string)GetValue(TekstPropProperty); }
+            set { SetValue(TekstPropProperty, value); }
+        }
+        public static readonly DependencyProperty TekstPropProperty =
+            DependencyProperty.Register("TekstProp", typeof(string), typeof(UserControl1), new PropertyMetadata( null, new PropertyChangedCallback(OnCurrentReadingChanged)));
+
+        //tabela/lista z danymi dla DataGrid
+        public IEnumerable SourceProp
+        {
+            get { return (IEnumerable)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+        public static readonly DependencyProperty SourceProperty =
+            DependencyProperty.Register("SourceProp", typeof(IEnumerable), typeof(UserControl1));
+
+       
+        private static void OnCurrentReadingChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            UserControl1 u = (UserControl1)d;
+            if (u.dvSource != null)
+            {
+                string query = $"name LIKE '%{u.TekstProp}%'";
+                u.dvSource.RowFilter = query;
+            }
+        }
+
+        public DataView dvSource
+        {
+            get { return (DataView)GetValue(dvSourceProperty); }
+            set { SetValue(dvSourceProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for dvSource.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty dvSourceProperty =
+            DependencyProperty.Register("dvSource", typeof(DataView), typeof(UserControl1));
+
+        
+        public string tekstSelected
+        {
+            get { return (string)GetValue(tekstSelectedProperty); }
+            set { SetValue(tekstSelectedProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for tekstSelected.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty tekstSelectedProperty =
+            DependencyProperty.Register("tekstSelected", typeof(string), typeof(UserControl1));
+
+        public DataRow rowSelected
+        {
+            get { return (DataRow)GetValue(rowSelectedProperty); }
+            set { SetValue(rowSelectedProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for rowSelected.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty rowSelectedProperty =
+            DependencyProperty.Register("rowSelected", typeof(DataRow), typeof(UserControl1), new PropertyMetadata(null, new PropertyChangedCallback( onRowSelected)));
+
+        private static void onRowSelected(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            MessageBox.Show("DUPA");
+           //UserControl1 u = (UserControl1)d;
+           // if (u.dvSource != null)
+           // {
+           //     MessageBox.Show(u.rowSelected["name"].ToString());
+           //     //u.tekstSelected = u.rowSelected["name"].ToString();
+           // }
+        }
+
+    }
+}
